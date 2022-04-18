b984c85eb579ecbd052af0f724c8b5bd4adf9e5c
carlosvico <calladrianjr@gmail.com>
2022-04-17 23:14:32 -0300

    exercicio 10 do trello acrescentado

---
+++ M2S3/ex-trello/ex10/script.js
@@ -0,0 +1,9 @@
+var misturaTudo = (arrayOriginal, segundoArray) => {
+    
+    return arrayOriginal.push(segundoArray);
+}
+
+let arrayNumerica = [1,2,3];
+let arraySilabica = ['ba','fa','la'];
+
+console.log(misturaTudo(arrayNumerica, arraySilabica));
